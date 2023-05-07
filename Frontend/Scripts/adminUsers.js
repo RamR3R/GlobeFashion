73eaa6348f5117e4524105a2ef35eb648f515d72
RamR3R <sendtoramsundar@gmail.com>
2023-05-08 01:24:36 +0530

    Admin Users done

--- Frontend/Scripts/adminUsers.js
+++ Frontend/Scripts/adminUsers.js
@@ -1,11 +1,29 @@
 const baseUrl = `http://localhost:3030/users/`;
 
-const userTbody = document.getElementById("user-tbody");
-
-console.log(userTbody);
-
 fetch(baseUrl)
 .then(res=>res.json())
 .then(data=>{
-    console.log(data);
+    console.log(data.data);
+    displayUsers(data.data);
 })
+
+
+function displayUsers(data)
+{
+    console.log(data);
+    let userTbody = document.getElementById("user-tbody");
+    data.forEach((item)=>{
+        let tr = document.createElement("tr");
+        tr.innerHTML = renderTable(item);
+
+        userTbody.append(tr);
+    });
+}
+
+function renderTable(item)
+{
+    return `<td>${item.name}</td>
+    <td>${item.email}</td>
+    <td>${item.noOfOrder}</td>
+    <td>${item.admin}</td>`
+}
